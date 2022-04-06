bd11e3d281afc73b98e8fca1c864a6c3c83f2611
Dave <87390532+vadedes@users.noreply.github.com>
2022-04-06 19:14:18 +0800

    completed slider project

---
+++ 07-slider/src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
+  useEffect(() => {
+    //automatically set index every 5secs
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          //destructure data
+          const { id, image, name, title, quote } = person;
+
+          //set slide position conditions
+          let position = 'nextSlide';
+
+          //if the person's index is the current index
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+
+          //if the person's index is at the end
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev' onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
+
+/* 
+Steps:
+  1. set initial states for people and index
+  2. set 3 ways to move slides
+    - autoslide using useEffect
+    - onClick of buttons PREV and NEXT
+  3. loop thru data and destructure data values to be used
+  4. Set conditions for dynamic classname of each article
+    - nextSlide, activeSlide and lastSlide
+  5. article element dynamic, left and right buttons are static
+
+*/
